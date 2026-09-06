c8f9289dd02cb4d9a4596f99ed1f81cd91bf0769
vasim786momin <vasim786diwan@gmail.com>
2026-09-06 14:28:03 +0530

    q3.dart
    q3.dart code

---
+++ lib/q3.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  print(10/2); // divide operation is perfomr ->5.0
+
+  print(17~/5);  // print integer 3 . '~' use to round of the value
+
+  print(17 % 5);  // it print 2 . print reminder
+
+
+  int a=7;
+  int b=2;
+  print('Result: ${a/b}'); // print 3.5 ,divide operation perform ${a/b}
+
+
+  var x=5;
+  print('value is $x +1');  // it  print 'value is 5+1 
+  
+  // correct syntax for addition is 
+  //print('value is ${x+1}');
+} 
+
+
