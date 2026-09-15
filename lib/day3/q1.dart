029385c32a6d0dcf32f668a031aa80880129505f
vasim786momin <vasim786diwan@gmail.com>
2026-09-15 20:16:35 +0530

    Day 03
    q1.dart

---
+++ lib/day3/q1.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  var fruits=['apple','banana','mango'];
+  print(fruits.length); // it print 3.
+
+  print(fruits[1]); //it print banana.
+
+  print(fruits.contains('grapes')); // it print false
+
+}
