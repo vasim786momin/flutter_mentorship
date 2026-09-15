b4d537ff48790daddb4c88a58b36e91495b42b87
vasim786momin <vasim786diwan@gmail.com>
2026-09-15 20:38:37 +0530

    Day 03
    q1.dart

--- lib/day3/q1.dart
+++ lib/day3/q1.dart
@@ -1,10 +1,37 @@
 void main(){
-
+   // (a)
   var fruits=['apple','banana','mango'];
   print(fruits.length); // it print 3.
-
   print(fruits[1]); //it print banana.
-
   print(fruits.contains('grapes')); // it print false
 
+
+ 
+ //(b)
+
+   var nums=[10,20,30];
+  nums.add(40);  // nums=[10,20,30,40]
+  nums.remove(20); // nums=[10,30,40]
+  print(nums); // it print nums=[10,30,40]
+  print(nums.length); // it print 3
+
+
+
+//(c)
+
+var user={'name':'Priya','age':25};
+print(user['name']);  // it print priya
+print(user['email']);  // it print null , kay 'emal' is not present
+
+
+//(d)
+
+var scores=[5,10,15];
+for (int i=0;i<scores.length;i++){
+  print(scores[i]*2);  // it print 10,20,30
+}
+
+
+
+ 
 }
